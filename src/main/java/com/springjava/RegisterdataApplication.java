dc1b8f1e71cc00f1a2ede8e4fc01476a2f6decaf
Rushikeshpatil1890 <130634429+Rushikeshpatil1890@users.noreply.github.com>
2024-03-12 11:27:42 +0530

    First Commit

---
+++ src/main/java/com/springjava/RegisterdataApplication.java
@@ -0,0 +1,13 @@
+package com.springjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterdataApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/springjava/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package com.springjava.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.springjava.entity.User;
+public interface UserRepository extends JpaRepository<User, Integer> {
+User findByEmail(String email);
+}
